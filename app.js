e78bc145fab9b0ef4f285116fdc14aaec8f097bb
Joey <101076758+jjjooeyyy@users.noreply.github.com>
2022-07-31 22:32:14 +0100

    updated

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // DOM 
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result >p");
 const rock_div = document.getElementById("rock");
 const scissors_div = document.getElementById("scissors");
 const paper_div = document.getElementById("paper");
@@ -22,21 +22,50 @@ switch (userChoice + computerChoice) {
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
-        console.log("win");
+        win(userChoice,computerChoice);
         break;
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
-        console.log("lose");
+        lose(userChoice,computerChoice);
         break;
     case "rockrock":
     case " scissorsscissors":
     case "paperpaper":
-        console.log(draw);
+        draw(userChoice,computerChoice);
         break;
 }
 }
 
+//What will happen to scoreboard and message when win 
+function win(userChoice,computerChoice) {
+userScore++; 
+userScore_span.innerHTML = userScore; //score board
+computerScore_span.innerHTML = computerScore; //score board
+const tinyUserWord = "user".fontsize(1).sup();
+const tinyCompWord = "comp".fontsize(1).sup();
+result_p.innerHTML = userChoice + tinyUserWord +  " beats " + computerChoice + tinyCompWord + ". YOU WIN!"; // change message
+}
+
+//What will happen to scoreboard and message when lose 
+function lose(userChoice,computerChoice) {
+computerScore++;
+const tinyUserWord = "user".fontsize(1).sup();
+const tinyCompWord = "comp".fontsize(1).sup();
+computerScore_span.innerHTML = computerScore; //score board
+userScore_span.innerHTML = userScore; //score board
+document.getElementById()
+result_p.innerHTML = computerChoice + tinyCompWord +  " beats " + userChoice + tinyUserWord + ". YOU LOSE!"; // change message
+}
+
+//What will happen to scoreboard and message when draw
+function draw(userChoice,computerChoice) {
+computerScore_span.innerHTML = computerScore; //score board
+userScore_span.innerHTML = userScore; //score board
+const tinyUserWord = "user".fontsize(1).sup();
+const tinyCompWord = "comp".fontsize(1).sup();
+result_p.innerHTML = userChoice + tinyUserWord + " meets " + computerChoice + tinyCompWord + ". It's a DRAW!"; 
+}
 
 
 function main() {
@@ -53,3 +82,6 @@ function main() {
 
 }
 
+
+
+main();
